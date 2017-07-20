c7da5304e81169377469414c419b8fc100e35e72
hidekisdouglas <hidekisdouglas@outlook.com>
2017-07-20 11:17:15 -0300

    Criação da class DALCategoria para implentação dos metodos para acessar o banco(incluir, alterar e exlcuir)

---
+++ ControleDeEstoque/DAL/DALCategoria.cs
@@ -0,0 +1,56 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class DALCategoria
+    {
+        private DALConexao conexao;
+        //Construtor
+        public DALCategoria(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloCategoria modelo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            
+            cmd.CommandText = "insert into categoria(cat_nome) values (@nome); select @@IDENTITY";
+            cmd.Parameters.AddWithValue("@nome", modelo.CatNome);
+            conexao.Connectar();
+            modelo.CatCod = Convert.ToInt32(cmd.ExecuteScalar());
+            conexao.Desconectar();
+        }
+        public void Alterar(ModeloCategoria modelo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+
+            cmd.CommandText = "update categoria set cat_nome = @nome; where cat_cod = @codigo";
+            cmd.Parameters.AddWithValue("@nome", modelo.CatNome);
+            cmd.Parameters.AddWithValue("@codigo", modelo.CatCod);
+            conexao.Connectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+        public void Excluir (int codigo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+
+            cmd.CommandText = "delete from categoria where cat_cod = @codigo";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Connectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+
+    }
+}
