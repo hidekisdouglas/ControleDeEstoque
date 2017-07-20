3c3cf1d5d64ae1c8e784148ae6a0ba4da13847c9
hidekisdouglas <hidekisdouglas@outlook.com>
2017-07-20 15:58:34 -0300

    Criação do frmConsultaCategoria

---
+++ ControleDeEstoque/GUI/frmConsultaCategoria.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GUI
+{
+    partial class frmConsultaCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btLocalizar = new System.Windows.Forms.Button();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Categoria";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(13, 30);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(431, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // btLocalizar
+            // 
+            this.btLocalizar.Location = new System.Drawing.Point(450, 28);
+            this.btLocalizar.Name = "btLocalizar";
+            this.btLocalizar.Size = new System.Drawing.Size(75, 23);
+            this.btLocalizar.TabIndex = 2;
+            this.btLocalizar.Text = "Localizar";
+            this.btLocalizar.UseVisualStyleBackColor = true;
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.AllowUserToAddRows = false;
+            this.dgvDados.AllowUserToDeleteRows = false;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(13, 81);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.ReadOnly = true;
+            this.dgvDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDados.Size = new System.Drawing.Size(759, 430);
+            this.dgvDados.TabIndex = 3;
+            // 
+            // frmConsultaCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.btLocalizar);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Name = "frmConsultaCategoria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta categoria";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btLocalizar;
+        private System.Windows.Forms.DataGridView dgvDados;
+    }
+}
