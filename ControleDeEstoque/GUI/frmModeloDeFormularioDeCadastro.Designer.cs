52fac7ffb024f14d04a26e907b8d7ee5374760d3
hidekisdouglas <hidekisdouglas@outlook.com>
2017-07-20 13:21:51 -0300

    Criação do novo frmModeloDeFormularioDeCadastro e inclusão dos botões

---
+++ ControleDeEstoque/GUI/frmModeloDeFormularioDeCadastro.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace GUI
+{
+    partial class frmModeloDeFormularioDeCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnDados = new System.Windows.Forms.Panel();
+            this.pnBotoes = new System.Windows.Forms.Panel();
+            this.btInserir = new System.Windows.Forms.Button();
+            this.Localizar = new System.Windows.Forms.Button();
+            this.btAlterar = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.pnBotoes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnDados
+            // 
+            this.pnDados.Location = new System.Drawing.Point(13, 13);
+            this.pnDados.Name = "pnDados";
+            this.pnDados.Size = new System.Drawing.Size(759, 423);
+            this.pnDados.TabIndex = 0;
+            // 
+            // pnBotoes
+            // 
+            this.pnBotoes.Controls.Add(this.btCancelar);
+            this.pnBotoes.Controls.Add(this.btSalvar);
+            this.pnBotoes.Controls.Add(this.btExcluir);
+            this.pnBotoes.Controls.Add(this.btAlterar);
+            this.pnBotoes.Controls.Add(this.Localizar);
+            this.pnBotoes.Controls.Add(this.btInserir);
+            this.pnBotoes.Location = new System.Drawing.Point(13, 443);
+            this.pnBotoes.Name = "pnBotoes";
+            this.pnBotoes.Size = new System.Drawing.Size(759, 106);
+            this.pnBotoes.TabIndex = 1;
+            // 
+            // btInserir
+            // 
+            this.btInserir.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btInserir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btInserir.Image = global::GUI.Properties.Resources.Novo;
+            this.btInserir.Location = new System.Drawing.Point(13, 12);
+            this.btInserir.Name = "btInserir";
+            this.btInserir.Size = new System.Drawing.Size(82, 82);
+            this.btInserir.TabIndex = 0;
+            this.btInserir.Text = "Inserir";
+            this.btInserir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btInserir.UseVisualStyleBackColor = true;
+            this.btInserir.Click += new System.EventHandler(this.btInserir_Click);
+            // 
+            // Localizar
+            // 
+            this.Localizar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.Localizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Localizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Localizar.Image = global::GUI.Properties.Resources.localizar_fw;
+            this.Localizar.Location = new System.Drawing.Point(143, 12);
+            this.Localizar.Name = "Localizar";
+            this.Localizar.Size = new System.Drawing.Size(82, 82);
+            this.Localizar.TabIndex = 1;
+            this.Localizar.Text = "Localizar";
+            this.Localizar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Localizar.UseVisualStyleBackColor = true;
+            this.Localizar.Click += new System.EventHandler(this.Localizar_Click);
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btAlterar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAlterar.Image = global::GUI.Properties.Resources.Alterar;
+            this.btAlterar.Location = new System.Drawing.Point(273, 12);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(82, 82);
+            this.btAlterar.TabIndex = 2;
+            this.btAlterar.Text = "Alterar";
+            this.btAlterar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btAlterar.UseVisualStyleBackColor = true;
+            this.btAlterar.Click += new System.EventHandler(this.btAlterar_Click);
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExcluir.Image = global::GUI.Properties.Resources.Excluir;
+            this.btExcluir.Location = new System.Drawing.Point(403, 12);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(82, 82);
+            this.btExcluir.TabIndex = 3;
+            this.btExcluir.Text = "Excluir";
+            this.btExcluir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btExcluir.UseVisualStyleBackColor = true;
+            this.btExcluir.Click += new System.EventHandler(this.btExcluir_Click);
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSalvar.Image = global::GUI.Properties.Resources.Salvar1_fw;
+            this.btSalvar.Location = new System.Drawing.Point(533, 12);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(82, 82);
+            this.btSalvar.TabIndex = 4;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancelar.Image = global::GUI.Properties.Resources.Cancelar;
+            this.btCancelar.Location = new System.Drawing.Point(663, 12);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(82, 82);
+            this.btCancelar.TabIndex = 5;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // frmModeloDeFormularioDeCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pnBotoes);
+            this.Controls.Add(this.pnDados);
+            this.Name = "frmModeloDeFormularioDeCadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modelo de formulario de cadastro";
+            this.pnBotoes.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnDados;
+        private System.Windows.Forms.Panel pnBotoes;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.Button btAlterar;
+        private System.Windows.Forms.Button Localizar;
+        private System.Windows.Forms.Button btInserir;
+        private System.Windows.Forms.Button btCancelar;
+    }
+}
---
+++ ControleDeEstoque/GUI/frmModeloDeFormularioDeCadastro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmModeloDeFormularioDeCadastro : Form
+    {
+        public frmModeloDeFormularioDeCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btInserir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Localizar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btAlterar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
