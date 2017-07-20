028862640ccab8a5b9bafdfafd7d99f7f65a7bea
hidekisdouglas <hidekisdouglas@outlook.com>
2017-07-20 10:44:15 -0300

    Criação da class DadosDaConexao, onde retorna a StringDeConexao

---
+++ ControleDeEstoque/DAL/DadosDaConexao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DadosDaConexao
+    {
+        public static String StringDeConexao
+        {
+            get
+            {
+                return "Data Source=DOUGLAS-PC\\SQLEXPRESS;Initial Catalog=db_Agenda;Integrated Security=True";
+            }
+        }
+    }
+}
