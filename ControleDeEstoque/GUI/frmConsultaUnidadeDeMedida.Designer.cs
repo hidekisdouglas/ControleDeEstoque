6355b4f557fbc2c780e3c0fee052b729e9747904
hidekisdouglas <hidekisdouglas@outlook.com>
2017-07-21 16:26:32 -0300

    Criação do frmConsultaUnidadeDeMedida e seus metodos

---
+++ ControleDeEstoque/GUI/frmConsultaUnidadeDeMedida.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GUI
+{
+    partial class frmConsultaUnidadeDeMedida
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btLocalizar = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.AllowUserToAddRows = false;
+            this.dgvDados.AllowUserToDeleteRows = false;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(12, 99);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.ReadOnly = true;
+            this.dgvDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDados.Size = new System.Drawing.Size(760, 430);
+            this.dgvDados.TabIndex = 7;
+            this.dgvDados.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDados_CellDoubleClick);
+            // 
+            // btLocalizar
+            // 
+            this.btLocalizar.Location = new System.Drawing.Point(449, 46);
+            this.btLocalizar.Name = "btLocalizar";
+            this.btLocalizar.Size = new System.Drawing.Size(75, 23);
+            this.btLocalizar.TabIndex = 6;
+            this.btLocalizar.Text = "Localizar";
+            this.btLocalizar.UseVisualStyleBackColor = true;
+            this.btLocalizar.Click += new System.EventHandler(this.btLocalizar_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(12, 48);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(431, 20);
+            this.txtValor.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Unidade de medida";
+            // 
+            // frmConsultaUnidadeDeMedida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.btLocalizar);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Name = "frmConsultaUnidadeDeMedida";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta unidade de medida";
+            this.Load += new System.EventHandler(this.frmConsultaUnidadeDeMedida_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button btLocalizar;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ControleDeEstoque/GUI/frmConsultaUnidadeDeMedida.cs
@@ -0,0 +1,53 @@
+﻿using BLL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmConsultaUnidadeDeMedida : Form
+    {
+        public int codigo = 0;
+
+        public frmConsultaUnidadeDeMedida()
+        {
+            InitializeComponent();
+        }
+
+        private void btLocalizar_Click(object sender, EventArgs e)
+        {
+           
+                DALConexao cx = new DALConexao(DadosDaConexao.StringDeConexao);
+                BLLUnidadeDeMedida bll = new BLLUnidadeDeMedida(cx);
+                dgvDados.DataSource = bll.Localizar(txtValor.Text);
+        }
+
+        private void frmConsultaUnidadeDeMedida_Load(object sender, EventArgs e)
+        {
+            //Carregar todos as categorias ao abrir o formulario de consulta
+            btLocalizar_Click(sender, e);
+            //troca o header da tabela
+            dgvDados.Columns[0].HeaderText = "Código";
+            dgvDados.Columns[0].Width = 70;
+            dgvDados.Columns[1].HeaderText = "Und. de Media";
+            dgvDados.Columns[1].Width = 630;
+        }
+
+        private void dgvDados_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Seleção de toda a linha da tabela
+            if (e.RowIndex >= 0)
+            {
+                this.codigo = Convert.ToInt32(dgvDados.Rows[e.RowIndex].Cells[0].Value);
+                this.Close();
+            }
+        }
+    }
+}
