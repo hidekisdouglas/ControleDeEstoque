ea21c1f4cafd1b97e08e7cf35f7acae6fadccefa
hidekisdouglas <hidekisdouglas@outlook.com>
2017-07-21 15:58:29 -0300

    Criação da class ModeloUnidadeDeMedida

---
+++ ControleDeEstoque/Modelo/ModeloUnidadeDeMedida.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class ModeloUnidadeDeMedida
+    {
+        public ModeloUnidadeDeMedida()
+        {
+            this.UmedCod = 0;
+            this.UmedNome = "";
+        }
+        public ModeloUnidadeDeMedida(int umedcot, string umednome)
+        {
+            this.UmedCod = umedcot;
+            this.UmedNome = umednome;
+        }
+
+        private int umed_cod;
+        public int UmedCod
+        {
+            get { return this.umed_cod; }
+            set { this.umed_cod = value; }
+        }
+        private String umed_nome;
+        public String UmedNome
+        {
+            get { return this.umed_nome; }
+            set { this.umed_nome = value; }
+        }
+    }
+}
