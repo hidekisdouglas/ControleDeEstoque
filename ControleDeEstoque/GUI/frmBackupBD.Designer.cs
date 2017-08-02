5548a6058e6a4ab24571ee0904c0b00a74ef891a
hidekisdouglas <hidekisdouglas@outlook.com>
2017-08-02 18:00:05 -0300

    Inclussão de um novo projeto class Ferramenta com as classes buscaendereco e SQLServerBackup e a criação do frmBackupDB

---
+++ ControleDeEstoque/GUI/frmBackupBD.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GUI
+{
+    partial class frmBackupBD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBackupBancoDeDados = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btBackupBancoDeDados
+            // 
+            this.btBackupBancoDeDados.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBackupBancoDeDados.Location = new System.Drawing.Point(122, 48);
+            this.btBackupBancoDeDados.Name = "btBackupBancoDeDados";
+            this.btBackupBancoDeDados.Size = new System.Drawing.Size(242, 198);
+            this.btBackupBancoDeDados.TabIndex = 0;
+            this.btBackupBancoDeDados.Text = "Backup do banco de dados";
+            this.btBackupBancoDeDados.UseVisualStyleBackColor = true;
+            this.btBackupBancoDeDados.Click += new System.EventHandler(this.btBackupBancoDeDados_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(122, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 207);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Restaurar Banco de Dados";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmBackupBD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btBackupBancoDeDados);
+            this.Name = "frmBackupBD";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Backup de banco de dados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBackupBancoDeDados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ ControleDeEstoque/GUI/frmBackupBD.cs
@@ -0,0 +1,33 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmBackupBD : Form
+    {
+        public frmBackupBD()
+        {
+            InitializeComponent();
+        }
+
+        private void btBackupBancoDeDados_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog d = new SaveFileDialog();
+            d.Filter = "Backup Files |*.bak";
+            d.ShowDialog();
+            if (d.FileName != "")
+            {
+                string nomeBanco = DadosDaConexao.banco;
+                String conexao = @"Data Source=" + DadosDaConexao.servidor + "; Initial Catolog=master;User=" + DadosDaConexao.usuario + ";Password=" + DadosDaConexao.senha;
+            }
+        }
+    }
+}
