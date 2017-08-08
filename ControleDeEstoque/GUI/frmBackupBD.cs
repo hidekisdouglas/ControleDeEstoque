88684e6fa0c8856be1da737f1adf769b27ff0572
hidekisdouglas <hidekisdouglas@outlook.com>
2017-08-08 11:41:18 -0300

    Implementação do restaurar backup

--- ControleDeEstoque/GUI/frmBackupBD.cs
+++ ControleDeEstoque/GUI/frmBackupBD.cs
@@ -40,5 +40,28 @@ namespace GUI
                 MessageBox.Show(erro.Message);
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog d = new OpenFileDialog();
+                d.Filter = "Backup Files |*.bak";
+                d.ShowDialog();
+                if (d.FileName != "")
+                {
+                    String nomeBanco = DadosDaConexao.banco;
+                    String localBackup = d.FileName;
+                    String conexao = @"Data Source=" + DadosDaConexao.servidor + "; Initial Catolog=master;" +
+                        "User=" + DadosDaConexao.usuario + ";Password=" + DadosDaConexao.senha;
+                    SQLServerBackup.RestauraDatabase(conexao, nomeBanco, localBackup);
+                    MessageBox.Show("Backup restaurado com sucesso!");
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
     }
 }
