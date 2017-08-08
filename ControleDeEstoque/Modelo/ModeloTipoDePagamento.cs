fb3251623e08e27e87e159247fea3758d0e83f02
hidekisdouglas <hidekisdouglas@outlook.com>
2017-08-08 12:18:42 -0300

    Criação do ModeloTipoDePagamento

---
+++ ControleDeEstoque/Modelo/ModeloTipoDePagamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class ModeloTipoDePagamento
+    {
+        public ModeloTipoDePagamento()
+        {
+            this.TpaCod = 0;
+            this.TpatNome = "";
+        }
+        public ModeloTipoDePagamento(int tpacod, string tpanome)
+        {
+            this.TpaCod = tpacod;
+            this.TpatNome = tpa_nome;
+        }
+
+        private int tpa_cod;
+        public int TpaCod
+        {
+            get { return this.tpa_cod; }
+            set { this.tpa_cod = value; }
+        }
+        private String tpa_nome;
+        public String TpatNome
+        {
+            get { return this.tpa_nome; }
+            set { this.tpa_nome = value; }
+        }
+    }
+}
