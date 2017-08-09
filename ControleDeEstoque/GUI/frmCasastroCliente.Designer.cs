ca037dc0a3514ae76e944de653017c9830621d74
hidekisdouglas <hidekisdouglas@outlook.com>
2017-08-09 11:37:44 -0300

    Implementação do frmCadastroCliente

---
+++ ControleDeEstoque/GUI/frmCasastroCliente.Designer.cs
@@ -0,0 +1,431 @@
+﻿namespace GUI
+{
+    partial class frmCasastroCliente
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbFisica = new System.Windows.Forms.RadioButton();
+            this.rbJuridica = new System.Windows.Forms.RadioButton();
+            this.txtRazaoSocial = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCpfCnpj = new System.Windows.Forms.TextBox();
+            this.lbCpfCnpj = new System.Windows.Forms.Label();
+            this.txtRgIe = new System.Windows.Forms.TextBox();
+            this.lbRgIe = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtEndNumero = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtCel = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.pnDados.SuspendLayout();
+            this.pnBotoes.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnDados
+            // 
+            this.pnDados.Controls.Add(this.txtBairro);
+            this.pnDados.Controls.Add(this.label15);
+            this.pnDados.Controls.Add(this.txtEmail);
+            this.pnDados.Controls.Add(this.label13);
+            this.pnDados.Controls.Add(this.txtCel);
+            this.pnDados.Controls.Add(this.label12);
+            this.pnDados.Controls.Add(this.txtTel);
+            this.pnDados.Controls.Add(this.label11);
+            this.pnDados.Controls.Add(this.txtEndNumero);
+            this.pnDados.Controls.Add(this.label10);
+            this.pnDados.Controls.Add(this.txtEnd);
+            this.pnDados.Controls.Add(this.label9);
+            this.pnDados.Controls.Add(this.txtCidade);
+            this.pnDados.Controls.Add(this.label8);
+            this.pnDados.Controls.Add(this.txtEstado);
+            this.pnDados.Controls.Add(this.label7);
+            this.pnDados.Controls.Add(this.txtCep);
+            this.pnDados.Controls.Add(this.label6);
+            this.pnDados.Controls.Add(this.txtRgIe);
+            this.pnDados.Controls.Add(this.lbRgIe);
+            this.pnDados.Controls.Add(this.txtCpfCnpj);
+            this.pnDados.Controls.Add(this.lbCpfCnpj);
+            this.pnDados.Controls.Add(this.txtRazaoSocial);
+            this.pnDados.Controls.Add(this.label3);
+            this.pnDados.Controls.Add(this.groupBox1);
+            this.pnDados.Controls.Add(this.txtNome);
+            this.pnDados.Controls.Add(this.label2);
+            this.pnDados.Controls.Add(this.txtCodigo);
+            this.pnDados.Controls.Add(this.label1);
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            // 
+            // btLocalizar
+            // 
+            this.btLocalizar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            // 
+            // btInserir
+            // 
+            this.btInserir.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Enabled = false;
+            this.txtCodigo.Location = new System.Drawing.Point(13, 30);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(13, 88);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(342, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbJuridica);
+            this.groupBox1.Controls.Add(this.rbFisica);
+            this.groupBox1.Location = new System.Drawing.Point(128, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 37);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tipo de Cliente";
+            // 
+            // rbFisica
+            // 
+            this.rbFisica.AutoSize = true;
+            this.rbFisica.Location = new System.Drawing.Point(28, 15);
+            this.rbFisica.Name = "rbFisica";
+            this.rbFisica.Size = new System.Drawing.Size(54, 17);
+            this.rbFisica.TabIndex = 0;
+            this.rbFisica.TabStop = true;
+            this.rbFisica.Text = "Física";
+            this.rbFisica.UseVisualStyleBackColor = true;
+            // 
+            // rbJuridica
+            // 
+            this.rbJuridica.AutoSize = true;
+            this.rbJuridica.Location = new System.Drawing.Point(145, 14);
+            this.rbJuridica.Name = "rbJuridica";
+            this.rbJuridica.Size = new System.Drawing.Size(63, 17);
+            this.rbJuridica.TabIndex = 1;
+            this.rbJuridica.TabStop = true;
+            this.rbJuridica.Text = "Jurídica";
+            this.rbJuridica.UseVisualStyleBackColor = true;
+            // 
+            // txtRazaoSocial
+            // 
+            this.txtRazaoSocial.Location = new System.Drawing.Point(388, 30);
+            this.txtRazaoSocial.Name = "txtRazaoSocial";
+            this.txtRazaoSocial.Size = new System.Drawing.Size(357, 20);
+            this.txtRazaoSocial.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(385, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Razão Social";
+            // 
+            // txtCpfCnpj
+            // 
+            this.txtCpfCnpj.Location = new System.Drawing.Point(388, 88);
+            this.txtCpfCnpj.Name = "txtCpfCnpj";
+            this.txtCpfCnpj.Size = new System.Drawing.Size(357, 20);
+            this.txtCpfCnpj.TabIndex = 8;
+            // 
+            // lbCpfCnpj
+            // 
+            this.lbCpfCnpj.AutoSize = true;
+            this.lbCpfCnpj.Location = new System.Drawing.Point(385, 69);
+            this.lbCpfCnpj.Name = "lbCpfCnpj";
+            this.lbCpfCnpj.Size = new System.Drawing.Size(27, 13);
+            this.lbCpfCnpj.TabIndex = 7;
+            this.lbCpfCnpj.Text = "CPF";
+            // 
+            // txtRgIe
+            // 
+            this.txtRgIe.Location = new System.Drawing.Point(388, 153);
+            this.txtRgIe.Name = "txtRgIe";
+            this.txtRgIe.Size = new System.Drawing.Size(357, 20);
+            this.txtRgIe.TabIndex = 10;
+            // 
+            // lbRgIe
+            // 
+            this.lbRgIe.AutoSize = true;
+            this.lbRgIe.Location = new System.Drawing.Point(385, 134);
+            this.lbRgIe.Name = "lbRgIe";
+            this.lbRgIe.Size = new System.Drawing.Size(23, 13);
+            this.lbRgIe.TabIndex = 9;
+            this.lbRgIe.Text = "RG";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(13, 270);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(60, 20);
+            this.txtCep.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "CEP";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(291, 270);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(64, 20);
+            this.txtEstado.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(288, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Estado";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(388, 270);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(357, 20);
+            this.txtCidade.TabIndex = 16;
+            this.txtCidade.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(385, 253);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Cidade";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(13, 319);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(602, 20);
+            this.txtEnd.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(10, 302);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Endereço";
+            // 
+            // txtEndNumero
+            // 
+            this.txtEndNumero.Location = new System.Drawing.Point(621, 319);
+            this.txtEndNumero.Name = "txtEndNumero";
+            this.txtEndNumero.Size = new System.Drawing.Size(124, 20);
+            this.txtEndNumero.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(618, 302);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Número";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(13, 213);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(342, 20);
+            this.txtTel.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(10, 196);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(49, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Telefone";
+            // 
+            // txtCel
+            // 
+            this.txtCel.Location = new System.Drawing.Point(388, 213);
+            this.txtCel.Name = "txtCel";
+            this.txtCel.Size = new System.Drawing.Size(357, 20);
+            this.txtCel.TabIndex = 24;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(385, 194);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(39, 13);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Celular";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(13, 153);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(342, 20);
+            this.txtEmail.TabIndex = 26;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(10, 136);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(32, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Email";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(81, 270);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(203, 20);
+            this.txtBairro.TabIndex = 30;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(78, 253);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(34, 13);
+            this.label15.TabIndex = 29;
+            this.label15.Text = "Bairro";
+            // 
+            // frmCasastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Name = "frmCasastroCliente";
+            this.Text = "Cadastro de Cliente";
+            this.pnDados.ResumeLayout(false);
+            this.pnDados.PerformLayout();
+            this.pnBotoes.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEndNumero;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRgIe;
+        private System.Windows.Forms.Label lbRgIe;
+        private System.Windows.Forms.TextBox txtCpfCnpj;
+        private System.Windows.Forms.Label lbCpfCnpj;
+        private System.Windows.Forms.TextBox txtRazaoSocial;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbJuridica;
+        private System.Windows.Forms.RadioButton rbFisica;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtCel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ ControleDeEstoque/GUI/frmCasastroCliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmCasastroCliente : GUI.frmModeloDeFormularioDeCadastro
+    {
+        public frmCasastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
